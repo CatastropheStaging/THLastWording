cfd78f0160bc348b226a5bf8f22ecedbf975a80e
CatastropheStaging <roundaboutmethod985@gmail.com>
2024-12-30 22:14:40 -0500

    Initial Commit

---
+++ lang-en-lw/patch.js
@@ -0,0 +1,13 @@
+{
+	"id": "lastwording",
+	"title": "English (Last Wording)",
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order"
+	],
+	"update": true,
+	"servers": [
+		"https://raw.githubusercontent.com/CatastropheStaging/THLastWording/master/"
+	]
+}
